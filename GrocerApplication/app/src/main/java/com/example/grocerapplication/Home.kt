03f72b27ca75a7a7191aad2167c952658a816008
Jordan <jcedeno870@gmail.com>
2022-10-10 16:00:07 -0500

    Add to List has some functionality, need to make textView dynamic

--- GrocerApplication/app/src/main/java/com/example/grocerapplication/Home.kt
+++ GrocerApplication/app/src/main/java/com/example/grocerapplication/Home.kt
@@ -5,8 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.PopupMenu
+import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import org.w3c.dom.Text
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,6 +26,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Home : Fragment() {
+    lateinit var addFAB: ExtendedFloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,9 +34,26 @@ class Home : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
-
-
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val textView: TextView = view.findViewById(R.id.home)
+        addFAB = view.findViewById(R.id.addList)
 
-}
+        addFAB.setOnClickListener{
+            val popupMenu:PopupMenu = PopupMenu(activity, addFAB)
+            popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener ( PopupMenu.OnMenuItemClickListener{ item ->
+                when(item.itemId){
+                    R.id.rice ->
+                        textView.text = item.title
+                    R.id.apple ->
+                        textView.text = item.title
+                }
+                true
+            })
+            popupMenu.show()
+            }
+        }
+    }
